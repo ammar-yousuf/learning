0b9c97a71aa459d75a5537f61568427949e00567
AMMAR YOUSUF <revhigh007@gmail.com>
2014-08-02 23:12:22 -0400

    Added a StoreController and played around with the mg-controller directive in the HTML markup.

--- angular/exercise1/app.js
+++ angular/exercise1/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('gemStore', [ ]);
+(function(){
+	var app = angular.module('gemStore', [ ]);
+
+	app.controller('StoreController', function(){
+		// Set a property of this controller to the gem we have defined below
+		// Once we have this defined we can access the data in the HTML
+		this.product = gem;
+	});
+
+	var gem = {
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: '....',
+	}
+
+})();
